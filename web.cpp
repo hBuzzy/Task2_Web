73c4a6dbb7cd6e30906dec23da32e45a38329f98
pahanmar <109517190+pahanmar@users.noreply.github.com>
2023-11-30 08:43:46 +0300

    Update web.cpp
    1)Заметил что не везде пробелы правильно стоят. Исправил

--- web.cpp
+++ web.cpp
@@ -12,12 +12,12 @@ void Web::drawWeb(QPainter *painter, const QPoint &StartPoint, bool drawCheck) {
         QPoint pointsOfWeb[kWebPointCount];
         double angel = 0;
         for (int i {0}; i < kWebPointCount; i++) {
-               angel = angel + (M_PI/kDivisorOfPi);
+               angel = angel + (M_PI / kDivisorOfPi);
                pointsOfWeb[i].setX(startPointX + kLineLength * cos(angel));
                pointsOfWeb[i].setY(startPointY + kLineLength * sin(angel));
             }
             painter->drawLine(startPointX, startPointY, pointsOfWeb[0].x(), pointsOfWeb[0].y());
-            painter->drawLine(pointsOfWeb[0].x(), pointsOfWeb[0].y(), pointsOfWeb[kWebPointCount - 1].x(), pointsOfWeb[kWebPointCount-1].y());
+            painter->drawLine(pointsOfWeb[0].x(), pointsOfWeb[0].y(), pointsOfWeb[kWebPointCount - 1].x(), pointsOfWeb[kWebPointCount - 1].y());
             for(int i {1}; i < kWebPointCount; i++){
             painter->drawLine(startPointX, startPointY, pointsOfWeb[i].x(), pointsOfWeb[i].y());
             painter->drawLine(pointsOfWeb[i - 1].x(), pointsOfWeb[i - 1].y(), pointsOfWeb[i].x(), pointsOfWeb[i].y());
