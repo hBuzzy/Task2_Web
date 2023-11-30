afe54e521995c8da20d37a30437b7fbd13a96b4e
pahanmar <109517190+pahanmar@users.noreply.github.com>
2023-11-30 08:39:29 +0300

    Update web.cpp
    1)Исправлены стилистические ошибки

--- web.cpp
+++ web.cpp
@@ -1,27 +1,28 @@
 #include "web.h"
 
-Web::Web(){}
+Web::Web() {}
 
-void Web::drawWeb(QPainter *painter, const QPoint &StartPoint, bool drawCheck){
-    {
-        if(drawCheck==true){
-        int startPointX=StartPoint.x();
-        int startPointY=StartPoint.y();
-        const int lineLength = 50;
-        const int webPointCount = 8;
-        QPoint pointsOfWeb[webPointCount];
+void Web::drawWeb(QPainter *painter, const QPoint &StartPoint, bool drawCheck) {
+        if (drawCheck == true) {
+        int startPointX = StartPoint.x();
+        int startPointY = StartPoint.y();
+        const int kLineLength = 50;
+        const int kWebPointCount = 8;
+        const int kDivisorOfPi = 4;
+        QPoint pointsOfWeb[kWebPointCount];
         double angel = 0;
-        for(int i {0}; i < webPointCount; i++){
-               angel=angel+(M_PI/4);
-               pointsOfWeb[i].setX(startPointX+lineLength*cos(angel));
-               pointsOfWeb[i].setY(startPointY+lineLength*sin(angel));
+        for (int i {0}; i < kWebPointCount; i++) {
+               angel = angel + (M_PI/kDivisorOfPi);
+               pointsOfWeb[i].setX(startPointX + kLineLength * cos(angel));
+               pointsOfWeb[i].setY(startPointY + kLineLength * sin(angel));
             }
-            painter->drawLine(startPointX, startPointY,pointsOfWeb[0].x(), pointsOfWeb[0].y());
-            painter->drawLine(pointsOfWeb[0].x(), pointsOfWeb[0].y(),pointsOfWeb[webPointCount-1].x(), pointsOfWeb[webPointCount-1].y());
-            for(int i {1}; i < webPointCount; i++){
-            painter->drawLine(startPointX, startPointY,pointsOfWeb[i].x(), pointsOfWeb[i].y());
-            painter->drawLine(pointsOfWeb[i-1].x(), pointsOfWeb[i-1].y(),pointsOfWeb[i].x(), pointsOfWeb[i].y());
+            painter->drawLine(startPointX, startPointY, pointsOfWeb[0].x(), pointsOfWeb[0].y());
+            painter->drawLine(pointsOfWeb[0].x(), pointsOfWeb[0].y(), pointsOfWeb[kWebPointCount - 1].x(), pointsOfWeb[kWebPointCount-1].y());
+            for(int i {1}; i < kWebPointCount; i++){
+            painter->drawLine(startPointX, startPointY, pointsOfWeb[i].x(), pointsOfWeb[i].y());
+            painter->drawLine(pointsOfWeb[i - 1].x(), pointsOfWeb[i - 1].y(), pointsOfWeb[i].x(), pointsOfWeb[i].y());
             }
-        } else{return;}
-    }
+        } else {
+            return;
+        }
 }
